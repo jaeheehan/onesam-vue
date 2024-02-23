a6621bf1fbc65ccb8241663c111353459e654be8
jaehee <jaehee23@naver.com>
2024-02-24 06:41:24 +0900

    chap8 teleport

---
+++ chap08/provide-inject-teleport-test/src/components/SongListItem.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "SongListItem",
+  inject : ["icons"],
+  props: ["song"]
+}
+</script>
+
+<template>
+  <li>
+    <i :class="song.done ? icons.checked : icons.unchecked"></i>
+    {{song.title}}
+  </li>
+</template>
+
+<style scoped>
+
+</style>
