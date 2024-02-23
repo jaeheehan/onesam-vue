e821b204e86cc1de2d862e7c6461236a1c35f7b4
jaehee <jaehee23@naver.com>
2024-02-24 05:56:59 +0900

    chap8 v-model 디렉티브

---
+++ chap08/v-model-test/src/components/InputName.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "InputName",
+  props: ["name"]
+}
+</script>
+
+<template>
+  <input type="text" :value="name" @input="$emit('update:name', $event.target.value)"/>
+</template>
+
+<style scoped>
+
+</style>
