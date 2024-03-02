cf2ecf94a9d6eb5203345e01c75cc6081583b670
jaehee <jaehee23@naver.com>
2024-03-02 13:29:25 +0900

    chap12 todolist base

---
+++ chap12/todolist-app-router/src/pages/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col p-3">
+      <router-link to="/todos/add" class="btn btn-primary">할일 추가</router-link>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoItem v-for="todoItem in todoList" :key="todoItem.id" :todoItem="todoItem"/>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+import TodoItem from "@/pages/TodoItem.vue";
+import {inject} from "vue";
+
+const todoList = inject('todoList')
+</script>
